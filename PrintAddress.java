76d3181b788fb215ec8807a1ff31d5a12ba0c84f
sauravsingh12qw <109010297+sauravsingh12qw@users.noreply.github.com>
2025-08-06 15:54:23 +0530

    Create PrintAddress.java

---
+++ PrintAddress.java
@@ -0,0 +1,13 @@
+public class PrintAddress {
+    public static void main(String[] args) {
+        // Single print
+        System.out.print("Name: Saurav Singh, ");
+        System.out.print("City: Gorakhpur, ");
+        System.out.print("State: Uttar Pradesh\n");
+
+        // Multiple println
+        System.out.println("Name: Saurav Singh");
+        System.out.println("City: Gorakhpur");
+        System.out.println("State: Uttar Pradesh");
+    }
+}
